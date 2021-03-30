a20f98c4aeedea84d041a8a40a4a89c58c2e65dc
zdxzdxzdx <769371987@qq.com>
2021-03-30 18:04:31 +0800

    设置路由

---
+++ src/components/TrashDetail.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="trash-detail">
+        <h1>{{msg}}:{{$route.params.noteId}}</h1>
+    </div>
+</template>
+
+<script >
+
+  export default  {
+    name:'TrashDetail',
+      data(){
+        return {
+            msg:'回收站笔记详情页'
+        }
+      }
+  }
+</script>
+
+<style scoped lang="scss">
+    h1{
+        color: blue;
+    }
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,38 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Home from '../views/Home.vue'
-
+import HelloWorld from '@/components/HelloWorld.vue';
+import  Login from '@/components/Login.vue'
+import NotebookList from '@/components/NotebookList.vue';
+import NoteDetail from '@/components/NoteDetail.vue';
+import TrashDetail from '@/components/TrashDetail.vue';
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'HelloWorld',
+    component: HelloWorld
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path:'/login',
+    name:'Login',
+    component:Login
+  },
+
+  {
+    path:'/notebooks',
+    component:NotebookList
+  },
+  {
+    path:'/note/:noteId',
+    component:NoteDetail
+  },
+  {
+    path:'/trash/:noteId',
+    component:TrashDetail
   }
+
 ]
 
 const router = new VueRouter({
